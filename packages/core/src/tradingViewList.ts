94163318eb6f15557f2c11565e7953de11f67a07
Rudi Starcevic <rudi.starcevic@pm.me>
2023-12-24 17:39:58 +0800

    Add mining symbol lists and write them to separate files

--- packages/core/src/tradingViewList.ts
+++ packages/core/src/tradingViewList.ts
@@ -1,16 +1,31 @@
 import { miners } from "./symbolsList.js"
+import * as fs from "fs"
 
 export const main = () => {
-  const list: string[] = []
+  const minersBTC: string[] = []
+  const minersIa: string[] = []
+  const minersBoom: string[] = []
+
+  const currentWorkingDirectory = process.cwd()
+  // console.log(`Current working directory: ${currentWorkingDirectory}`)
+
+  const outDir = `${currentWorkingDirectory}/dist`
 
   for (const miner of miners) {
     const { symbol, exchange } = miner
 
-    // list.push(`${exchange}:${symbol}`)
-    console.log(`https://trade.boom.com.hk/allmarkets/newBuyOrderPlace.jsp?market=US&code=${symbol}&quantity=2`)
-    console.log("")
+    minersBTC.push(`${exchange}:${symbol}`)
+
+    if (miner.lists?.includes("ia")) {
+      minersIa.push(`${exchange}:${symbol}`)
+    }
+
+    if (miner.tradableAt?.includes("boom")) {
+      minersBoom.push(`${exchange}:${symbol}`)
+    }
   }
 
-  console.log(list.join(","))
-  console.log(list.join(","))
+  fs.appendFileSync(outDir + "/Miners-BTC.txt", minersBTC.join(","))
+  fs.appendFileSync(outDir + "/Miners-IA.txt", minersIa.join(","))
+  fs.appendFileSync(outDir + "/Miners-Boom.txt", minersBoom.join(","))
 }
