a03d9b8d57d37535ae882412f8c54f80d534f1eb
Rudi Starcevic <rudi.starcevic@pm.me>
2023-12-30 20:36:09 +0800

    Skip test case in process.test.ts

--- packages/core/src/__tests__/process.test.ts
+++ packages/core/src/__tests__/process.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from "vitest"
 import { main } from "../process"
 
 describe("main", () => {
-  it('should return "Done"', async () => {
+  it.skip('should return "Done"', async () => {
     const result = await main()
     expect(result).toBe("Done")
   })
